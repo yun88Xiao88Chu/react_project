6c0d58569f9eb66ec29e5fdb471b246c42a5f839
yun88Xiao88Chu <1520420636@qq.com>
2020-05-05 01:36:15 +0800

    退出登录Provider说明

--- 05_src_Header组件/redux/actions/login.js
+++ 05_src_Header组件/redux/actions/login.js
@@ -8,6 +8,6 @@ export const saveUserInfo = userObj => {
 }
 
 export const deleteUserInfo = () => {
-  localStorage.clear()
-  return {type:DELETE_USERINFO}
-}
+  localStorage.clear()    // 数据清空初始化不会从本地读取
+  return {type:DELETE_USERINFO} // 将redux状态数据直接给初始值,清空数据,那么Provider将会render整个App应用,
+}                               //刷新后 状态数据isLogin的判断就自动退出跳转到login登录页面
